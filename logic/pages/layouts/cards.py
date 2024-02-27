fec50223aecfd060bff52bf9c31ca6a801f43b45
Tom Fynes <52704137+Tom-Fynes@users.noreply.github.com>
2024-02-27 15:50:19 +0000

    Update cards.py

--- logic/pages/layouts/cards.py
+++ logic/pages/layouts/cards.py
@@ -1,3 +1,6 @@
+import os
+import base64
+from pathlib import Path
 from dash import html
 import dash_bootstrap_components as dbc
 
@@ -21,9 +24,12 @@ class Cards:
 
     def _get_skill_cards(self, card_details: dict):
 
+        image_path = self._get_image(card_details.get("image_name"))
+        encoded_image = base64.b64encode(open(image_path, 'rb').read())
+
         card = dbc.Card(
         [
-        dbc.CardImg(src=card_details.get("image_path"), top=True),
+        dbc.CardImg(src="data:image/png;base64,{}".format(encoded_image.decode()), top=True),
         dbc.CardBody(
             [
                 html.H4(card_details.get("title"), className="card-title"),
@@ -37,3 +43,10 @@ class Cards:
             style={"width": "25rem", "min-height": "18rem"},
         )
         return card
+    
+    def _get_image(self, image_name: str) -> str:
+        parent = Path(__file__).resolve().parents[1] 
+        image_path = os.path.join(parent, f"assets/images/{image_name}")
+
+        return image_path
+
